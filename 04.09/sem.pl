dce01e0781537c81988b5bfd7ba51a774e8b1926
Werozel <khaibulaev.mn@gmail.com>
2020-09-06 16:23:40 +0300

    Added 04.09 sem

---
+++ 04.09/sem.pl
@@ -0,0 +1,43 @@
+parent(ann, nick).
+parent(tom, nick).
+parent(tom, john).
+parent(nick, terry).
+parent(nick, bob).
+parent(terry, mary).
+parent(terry, ron).
+parent(john, phil).
+parent(john, max).
+parent(john, liz).
+parent(max, jinny).
+
+woman(ann).
+woman(liz).
+woman(jinny).
+
+man(tom).
+man(nick).
+man(john).
+man(terry).
+man(bob).
+man(phil).
+man(max).
+man(ron).
+
+brother(X, Y) :- 
+    parent(Z, X), 
+    parent(Z, Y), 
+    dif(X, Y), 
+    man(X).
+
+mother(X, Y) :-
+    parent(X, Y),
+    woman(X).
+
+grandfather(X, Y) :-
+    parent(X, Z),
+    parent(Z, Y),
+    man(X).
+
+ancestor(X, Y) :-
+    parent(X, Z), 
+    ancestor(Z, Y).
