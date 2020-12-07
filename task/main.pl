01cd1a4b8e3a019884b55d75811cbaca6205467e
Werozel <khaibulaev.mn@gmail.com>
2020-12-07 13:53:37 +0300

    added attributes map

--- task/main.pl
+++ task/main.pl
@@ -1,24 +1,31 @@
 % :- [facts].
 :- ['database.txt'].
+:- [map].
 
+% Получение списка всех аттрибутов в системе
+get_attribute_map(Sport, ResMap) :- findall(pair(X, Y), get_attributes(Sport, X, Y), ResList), make_map(ResList, ResMap).
+
+% Получение всех аттрибутов для спорта
 get_attributes(Sport, Attribute, Value) :- attribute(Sport, Attribute, Value).
 get_attributes(Sport, Attribute, Value) :- 
     inherit(Parent, Sport),
     get_attributes(Parent, Attribute, Value).
 
-
+% Получение конкретного аттрибута для спорта
 get_attribute_value(Sport, Attribute, Res) :- 
     get_attributes(Sport, Attribute, Res), 
     !.
+
+% Получение всех видов спорта
+get_sports(SportsSet) :- 
+    findall(X, get_sports_duplicates(X), Sports), 
+    list_to_set(Sports, SportsSet).
 
 get_sports_duplicates(X) :- 
     inherit(_, X), 
     \+ inherit(X, _).
 
-get_sports(SportsSet) :- 
-    findall(X, get_sports_duplicates(X), Sports), 
-    list_to_set(Sports, SportsSet).
-
+% Сохранение в базу данных
 save_all() :-
     tell('database.txt'),
     listing(inherit),
--- task/map.pl
+++ task/map.pl
@@ -1,5 +1,12 @@
 % map - list of pairs
 
+make_map([], []).
+make_map([pair(K, V)|T], [pair(K, V)|NewMap]) :- 
+    not(get(T, K, _)),
+    make_map(T, NewMap),
+    !.
+make_map([pair(K, V)|T], NewMap) :- make_map(T, NewMap).
+
 update([], pair(X, V), [pair(X, V)]).
 update([pair(X, _)|T], pair(X, VNew), [pair(X, VNew)|T]) :- !.
 update([X|T], E, [X|New]) :- update(T, E, New).
