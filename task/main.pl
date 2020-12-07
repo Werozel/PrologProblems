32f5dcaf285953d56d3807135c547d7ccb99c97c
Werozel <khaibulaev.mn@gmail.com>
2020-12-07 21:42:06 +0300

    added correct diff between sports

--- task/main.pl
+++ task/main.pl
@@ -2,14 +2,18 @@
 :- ['database.txt'].
 :- [map].
 
-% Получение списка всех аттрибутов в системе
-get_attribute_map(Sport, ResMap) :- findall(pair(X, Y), get_attributes(Sport, X, Y), ResList), make_map(ResList, ResMap).
-
-% Получение всех аттрибутов для спорта
-get_attributes(Sport, Attribute, Value) :- attribute(Sport, Attribute, Value).
+% Получение всех аттрибутов для спорта (i, o, o) или значения для спорта и его аттрибута (i, i, i).
 get_attributes(Sport, Attribute, Value) :- 
+    get_attribute_map(Sport, Map),
+    iterate_map(Map, Attribute, Value).
+
+% Получение списка всех уникальных аттрибутов
+get_attribute_map(Sport, ResMap) :- findall(pair(X, Y), get_attributes_duplicates(Sport, X, Y), ResList), make_map(ResList, ResMap).
+
+get_attributes_duplicates(Sport, Attribute, Value) :- attribute(Sport, Attribute, Value).
+get_attributes_duplicates(Sport, Attribute, Value) :- 
     inherit(Parent, Sport),
-    get_attributes(Parent, Attribute, Value).
+    get_attributes_duplicates(Parent, Attribute, Value).
 
 % Получение конкретного аттрибута для спорта
 get_attribute_value(Sport, Attribute, Res) :- 
@@ -26,14 +30,22 @@ get_sports_duplicates(X) :-
     \+ inherit(X, _).
 
 % Получение различия в конкретном атрибуте
-get_attribute_dif(Sport, Sport, _, "None") :- !.
-get_attribute_dif(FirstSport, SecondSport, Attribute, "None") :- 
-    get_attribute_value(FirstSport, Attribute, Value),
-    get_attribute_value(SecondSport, Attribute, Value).
-get_attribute_dif(FirstSport, SecondSport, Attribute, pair(FirstValue, SecondValue)) :-
-    get_attribute_value(FirstSport, Attribute, FirstValue),
-    get_attribute_value(SecondSport, Attribute, SecondValue),
+get_attribute_diff(Sport, Sport, _, "None") :- !.
+get_attribute_diff(FirstSport, SecondSport, Attribute, "None") :- 
+    get_attributes(FirstSport, Attribute, Value),
+    get_attributes(SecondSport, Attribute, Value).
+get_attribute_diff(FirstSport, SecondSport, Attribute, pair(FirstValue, SecondValue)) :-
+    get_attributes(FirstSport, Attribute, FirstValue),
+    get_attributes(SecondSport, Attribute, SecondValue),
     dif(FirstValue, SecondValue).
+get_attribute_diff(FirstSport, SecondSport, Attribute, pair(FirstValue, "None")) :-
+    get_attributes(FirstSport, Attribute, FirstValue),
+    \+ get_attributes(SecondSport, Attribute, _).
+get_attribute_diff(FirstSport, SecondSport, Attribute, pair("None", SecondValue)) :-
+    get_attributes(SecondSport, Attribute, SecondValue),
+    \+ get_attributes(FirstSport, Attribute, _).
+
+
 
 % Сохранение в базу данных
 save_all() :-
--- task/map.pl
+++ task/map.pl
@@ -1,11 +1,15 @@
 % map - list of pairs
 
+iterate_map([pair(Key, Val)], Key, Val) :- !.
+iterate_map([pair(Key, Val)|_], Key, Val).
+iterate_map([_|T], Key, Val) :- iterate_map(T, Key, Val).
+
 make_map([], []).
 make_map([pair(K, V)|T], [pair(K, V)|NewMap]) :- 
     not(get(T, K, _)),
     make_map(T, NewMap),
     !.
-make_map([pair(K, V)|T], NewMap) :- make_map(T, NewMap).
+make_map([pair(_, _)|T], NewMap) :- make_map(T, NewMap).
 
 update([], pair(X, V), [pair(X, V)]).
 update([pair(X, _)|T], pair(X, VNew), [pair(X, VNew)|T]) :- !.
@@ -17,7 +21,7 @@ add([X|T], E, [X|New]) :- add(T, E, New).
 
 get([], _, []) :- fail.
 get([pair(K, V)|_], K, V) :- !.
-get([X|T], K, V) :- get(T, K, V).
+get([_|T], K, V) :- get(T, K, V).
 
 remove([], _, []) :- fail.
 remove([pair(K, _)|T], K, T) :- !.
