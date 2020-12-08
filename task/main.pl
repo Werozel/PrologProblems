94a6a70eca537ef03d872572ae781e50162c8fc6
Werozel <khaibulaev.mn@gmail.com>
2020-12-08 19:18:58 +0300

    Added sports editing

--- task/main.pl
+++ task/main.pl
@@ -2,6 +2,11 @@
 :- ['database.txt'].
 :- [map].
 
+% Получение всех аттрибутов в системе
+get_all_attributes(Attributes) :- 
+    findall(Attribute, attribute(_, Attribute, _), AttributesList),
+    list_to_set(AttributesList, Attributes).
+
 % Получение всех аттрибутов для спорта (i, o, o) или значения для спорта и его аттрибута (i, i, i).
 get_attributes(Sport, Attribute, Value) :- 
     get_attribute_map(Sport, Map),
@@ -35,18 +40,18 @@ print_list([X|T]) :-
     print_list(T).
 
 % Получение различия в конкретном атрибуте
-get_attribute_diff(Sport, Sport, _, "None") :- !.
-get_attribute_diff(FirstSport, SecondSport, Attribute, "None") :- 
+get_attribute_diff(Sport, Sport, _, "No difference") :- !.
+get_attribute_diff(FirstSport, SecondSport, Attribute, none) :- 
     get_attributes(FirstSport, Attribute, Value),
     get_attributes(SecondSport, Attribute, Value).
 get_attribute_diff(FirstSport, SecondSport, Attribute, pair(FirstValue, SecondValue)) :-
     get_attributes(FirstSport, Attribute, FirstValue),
     get_attributes(SecondSport, Attribute, SecondValue),
     dif(FirstValue, SecondValue).
-get_attribute_diff(FirstSport, SecondSport, Attribute, pair(FirstValue, "None")) :-
+get_attribute_diff(FirstSport, SecondSport, Attribute, pair(FirstValue, none)) :-
     get_attributes(FirstSport, Attribute, FirstValue),
     \+ get_attributes(SecondSport, Attribute, _).
-get_attribute_diff(FirstSport, SecondSport, Attribute, pair("None", SecondValue)) :-
+get_attribute_diff(FirstSport, SecondSport, Attribute, pair(none, SecondValue)) :-
     get_attributes(SecondSport, Attribute, SecondValue),
     \+ get_attributes(FirstSport, Attribute, _).
 
@@ -55,19 +60,71 @@ print_attribute_diff(FirstSport, SecondSport) :-
     make_map(ResultList, ResultMap),
     print_difference(ResultMap).
 
+% Редактировать аттрибуты спорта
+edit_sport_attribute(Sport, Attribute, Value) :- 
+    attribute(Sport, Attribute, _),
+    retract(attribute(Sport, Attribute, _)),
+    assert(attribute(Sport, Attribute, Value)),
+    !.
+edit_sport_attribute(Sport, Attribute, Value) :-
+    assert(attribute(Sport, Attribute, Value)).
+
+delete_sport_attribute(Sport, Attribute) :-
+    attribute(Sport, Attribute, _),
+    retract(attribute(Sport, Attribute, _)).
+
+manage_edit(Sport) :- 
+    writeln("1 - Добавить/изменить значение аттрибута"),
+    writeln("2 - Удалить аттрибут"),
+    writeln("q - Вернуться"),
+    read(N),
+    manage_edit(Sport, N).
+manage_edit(Sport, 1) :- 
+    write("Введите аттрибут: "),
+    read(Attribute),
+    write("Введите значение: "),
+    read(Value),    % TODO read string
+    edit_sport_attribute(Sport, Attribute, Value).
+manage_edit(Sport, 2) :-
+    write("Введите аттрибут: "),
+    read(Attribute),
+    delete_sport_attribute(Sport, Attribute).
+manage_edit(_, q) :- 
+    start,
+    !.
+manage_edit(Sport, _) :- manage_edit(Sport).
+
+% Удаление спорта
+delete_sport(Sport) :-
+    attribute(Sport, Attribute, _),
+    retract(attribute(Sport, Attribute, _)),
+    retract(inherit(_, Sport)). % TODO add instance()
+
 % Интеракция с пользователем
 start :- 
     writeln("1 - Найти все аттрибуты определенного вида спорта"),
     writeln("2 - Найти значение аттрибута для определенного вида спорта"),
     writeln("3 - Найти различия между двумя видами спорта"),
     writeln("4 - Найти все зарегестрированные виды спорта"),
+    writeln("5 - Добавить новый вид спорта"),
+    writeln("6 - Удалить существующий вид спорта"),
+    writeln("7 - Редактировать существующий вид спорта"),
     read(N),
     execute(N). 
+
+% Добавление нового спорта
+add_sport(Sport) :- 
+    % TODO inherit
+    assert(instance(Sport)).
 
 execute(1) :- 
     write("Введите название спорта: "),
     read(Sport),
-    findall(pair(Attribute, Value), get_attributes(Sport, Attribute, Value), Map),
+    findall(
+        pair(Attribute, Value), 
+        get_attributes(Sport, Attribute, Value), 
+        Map
+    ),
     print_map(Map).
 execute(2) :- 
     write("Введите название спорта: "),
@@ -85,6 +142,19 @@ execute(3) :-
 execute(4) :-
     get_sports(Sports),
     print_list(Sports).
+execute(5) :-
+    write("Введите новый спорт: "),
+    read(Sport),
+    add_sport(Sport).
+execute(6) :-
+    write("Введите спорт: "),
+    read(Sport),
+    delete_sport(Sport).
+execute(7) :-
+    write("Введите спорт: "),
+    read(Sport),
+    manage_edit(Sport).
+execute(_) :- start.
 
 % Сохранение в базу данных
 save_all() :-
